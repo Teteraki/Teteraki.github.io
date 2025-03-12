d716a391a3eb3e4a46b0aa541c5f534263f1a53e
Dylan Sanders <dsand171@mtroyal.ca>
2025-03-12 14:03:44 -0600

    rewrote JS to be more code efficient, added images to img folder vs using discord cdn links - was causing issues loading the images.

---
+++ js/event-tile-factory.js
@@ -0,0 +1,35 @@
+export function createEventTiles(events, container) {
+    events.forEach(event => {
+        const tile = document.createElement("div");
+        tile.classList.add("flex", "flex-col", "items-center");
+      
+        // Create the anchor element to wrap the image
+        const imageLink = document.createElement("a");
+        imageLink.href = event.url; // Set the URL
+        imageLink.target = "_blank"; // Open the link in a new tab
+      
+        // Create the image element
+        const img = document.createElement("img");
+        img.classList.add("w-full", "h-auto");
+        img.src = event.image;
+        img.alt = event.name;
+        img.classList.add("border-2", "border-black");
+      
+        // Append the image to the anchor
+        imageLink.appendChild(img);
+      
+        // Create the anchor element for the event name
+        const a = document.createElement("a");
+        a.classList.add("mt-2", "text-blue-500", "hover:underline");
+        a.href = event.url; // Set the URL
+        a.target = "_blank"; // Open the link in a new tab
+        a.textContent = event.name;
+      
+        // Append the clickable image and anchor to the tile div
+        tile.appendChild(imageLink);
+        tile.appendChild(a);
+      
+        // Append the tile to the container
+        container.appendChild(tile);
+    });
+}
---
+++ js/events.js
@@ -0,0 +1,33 @@
+import { createEventTiles } from "./event-tile-factory.js";
+
+const upcoming_events = [
+    {
+      image:
+        "../img/events/abmc.webp",
+      name: "Alberta Melee Mountain Climb - Biweekly Netplay Sunday @ 7PM GMT",
+      url: "https://start.gg/abmc",
+    },
+    {
+      image:
+        "../img/events/btb.webp",
+      name: "Black Tie Basement - Biweekly Friday @ 6PM GMT",
+      url: "https://start.gg/btb",
+    },
+  
+    // Add more events as needed
+  ];
+  
+const past_events = [
+    { image: "../img/events/saltflats.png", name: "Salt Flats x Cold Hands Collective", url: "https://start.gg/saltflats" },
+    { image: "../img/events/fireplace.png", name: "The Fireplace", url: "https://start.gg/fire" },
+    
+    // Add more events as needed
+  ];
+  
+  // Create the upcoming events tiles and append them to the page.
+  const upcoming_container = document.getElementById("upcoming-events");
+  createEventTiles(upcoming_events, upcoming_container);
+
+  // Craete the past events tiles and append them to the page.
+  const past_container = document.getElementById("past-events");
+  createEventTiles(past_events, past_container)
