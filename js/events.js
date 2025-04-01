ac322dc29e18c034c7137dfe73184b87ed6b3ce1
Dylan <90537486+Teteraki@users.noreply.github.com>
2025-04-01 13:09:17 -0600

    Update events.js

--- js/events.js
+++ js/events.js
@@ -3,13 +3,13 @@ import { createEventTiles } from "./event-tile-factory.js";
 const upcoming_events = [
     {
       image:
-        "../img/events/abmc.webp",
+        "/img/events/abmc.webp",
       name: "Alberta Melee Mountain Climb - Biweekly Netplay Sunday's @ 7PM GMT",
       url: "https://start.gg/abmc",
     },
     {
       image:
-        "../img/events/btb.webp",
+        "/img/events/btb.webp",
       name: "Black Tie Basement - Biweekly Friday's @ 6PM GMT",
       url: "https://start.gg/btb",
     },
@@ -18,8 +18,8 @@ const upcoming_events = [
   ];
   
 const past_events = [
-    { image: "../img/events/saltflats.png", name: "Salt Flats x Cold Hands Collective", url: "https://start.gg/saltflats" },
-    { image: "../img/events/fireplace.png", name: "The Fireplace", url: "https://start.gg/fire" },
+    { image: "/img/events/saltflats.png", name: "Salt Flats x Cold Hands Collective", url: "https://start.gg/saltflats" },
+    { image: "/img/events/fireplace.png", name: "The Fireplace", url: "https://start.gg/fire" },
     
     // Add more events as needed
   ];
