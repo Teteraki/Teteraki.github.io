53ea930f99b79923b420c1886e7d6e6ac1b84667
Dylan S <dsand171@mtroyal.ca>
2025-04-16 17:21:04 -0600

    final touches for now

--- js/events.js
+++ js/events.js
@@ -1,33 +1,44 @@
 import { createEventTiles } from "/js/event-tile-factory.js";
 
 const upcoming_events = [
-    {
-      image:
-        "/img/events/abmc.webp",
-      name: "Alberta Melee Mountain Climb - Biweekly Netplay Sunday's @ 7PM GMT",
-      url: "https://start.gg/abmc",
-    },
-    {
-      image:
-        "/img/events/btb.webp",
-      name: "Black Tie Basement - Biweekly Friday's @ 6PM GMT",
-      url: "https://start.gg/btb",
-    },
-  
-    // Add more events as needed
-  ];
-  
-const past_events = [
-    { image: "/img/events/saltflats.png", name: "Salt Flats x Cold Hands Collective", url: "https://start.gg/saltflats" },
-    { image: "/img/events/fireplace.png", name: "The Fireplace", url: "https://start.gg/fire" },
-    
-    // Add more events as needed
-  ];
-  
-  // Create the upcoming events tiles and append them to the page.
-  const upcoming_container = document.getElementById("upcoming-events");
-  createEventTiles(upcoming_events, upcoming_container);
+  {
+    image: "/img/events/abmc.webp",
+    name: "Alberta Melee Mountain Climb - Biweekly Netplay Sunday's @ 7PM GMT",
+    url: "https://start.gg/abmc",
+  },
+  {
+    image: "/img/events/btb.webp",
+    name: "Black Tie Basement - Biweekly Friday's @ 6PM GMT",
+    url: "https://start.gg/btb",
+  },
+  {
+    image: "/img/events/mru",
+    name: "MRU Smash - Weekly Tuesday's @6:30 PM (Ran by MRU Smash Club)",
+    url: "https://start.gg/mrusmash",
+  },
 
-  // Craete the past events tiles and append them to the page.
-  const past_container = document.getElementById("past-events");
-  createEventTiles(past_events, past_container)
+  // Add more events as needed
+];
+
+const past_events = [
+  {
+    image: "/img/events/saltflats.png",
+    name: "Salt Flats x Cold Hands Collective",
+    url: "https://start.gg/saltflats",
+  },
+  {
+    image: "/img/events/fireplace.png",
+    name: "The Fireplace",
+    url: "https://start.gg/fire",
+  },
+
+  // Add more events as needed
+];
+
+// Create the upcoming events tiles and append them to the page.
+const upcoming_container = document.getElementById("upcoming-events");
+createEventTiles(upcoming_events, upcoming_container);
+
+// Craete the past events tiles and append them to the page.
+const past_container = document.getElementById("past-events");
+createEventTiles(past_events, past_container);
